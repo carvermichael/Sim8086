d12a4503282de3fff25211a96dcfc9610f3a3a6d
Michael Carver <carvermichael@gmail.com>
2023-08-05 19:45:40 -0500

    homework 2: making more movs!

--- main.go
+++ main.go
@@ -8,40 +8,41 @@ import (
 	"strings"
 )
 
-const (
-	MOV = iota
-	ADD
-)
-
-//const (
-//	MOV_BITS byte = 0b10001000
-//	MASK_DW  byte = 0b11111100
-//	D_BITS   byte = 0b00000010
-//	W_BITS   byte = 0b00000001
-//	REG_MASK byte = 0b00111000
-//	RM_MASK  byte = 0b00000111
-//)
-
+// TODO: this could be a much faster lookup as an array with the index
+//			being ((w << 3) | rem)
+// 			Perf test to compare?
 var w_on_map = map[byte]string{
-	0b000: "AX",
-	0b001: "CX",
-	0b010: "DX",
-	0b011: "BX",
-	0b100: "SP",
-	0b101: "BP",
-	0b110: "SI",
-	0b111: "DI",
+	0b000: "ax",
+	0b001: "cx",
+	0b010: "dx",
+	0b011: "bx",
+	0b100: "sp",
+	0b101: "bp",
+	0b110: "si",
+	0b111: "di",
 }
 
 var w_off_map = map[byte]string{
-	0b000: "AL",
-	0b001: "CL",
-	0b010: "DL",
-	0b011: "BL",
-	0b100: "AH",
-	0b101: "CH",
-	0b110: "DH",
-	0b111: "BH",
+	0b000: "al",
+	0b001: "cl",
+	0b010: "dl",
+	0b011: "bl",
+	0b100: "ah",
+	0b101: "ch",
+	0b110: "dh",
+	0b111: "bh",
+}
+
+// TODO: index into an array here
+var rm_effective_map = map[byte]string{
+	0b000: "bx + si",
+	0b001: "bx + di",
+	0b010: "bp + si",
+	0b011: "bp + di",
+	0b100: "si",
+	0b101: "di",
+	0b110: "bp",
+	0b111: "bx",
 }
 
 var prog []byte             // all bytes
@@ -50,12 +51,6 @@ var b byte                  // current byte
 var builder strings.Builder // builds ultimate output
 
 var err error
-
-/*
-	TODO: Assignment 2:
-		- Register to Memory + Memory to Register
-		- Immediate to Register
-*/
 
 func main() {
 
@@ -76,9 +71,12 @@ func main() {
 
 		if (b&0b11111100)^0b10001000 == 0 {
 			handleRegToFromRegMem()
+		} else if (b&0b11110000)^0b10110000 == 0 {
+			handleImmediateToReg()
 		}
 
 		i++
+		builder.WriteString("\n")
 	}
 	fmt.Print(builder.String())
 }
@@ -86,7 +84,7 @@ func main() {
 // MOV #1
 func handleRegToFromRegMem() {
 
-	builder.WriteString("MOV ")
+	builder.WriteString("mov ")
 
 	// get d, w, mod, reg, and r/m first
 	w_bit_on := (b & 0b00000001) != 0
@@ -96,7 +94,7 @@ func handleRegToFromRegMem() {
 	b2 := prog[i]
 
 	// MOD
-	//mod_bits := (b2 & 0b11000000) >> 6
+	mod_bits := (b2 & 0b11000000) >> 6
 
 	// REG
 	reg_bits := (b2 & 0b00111000) >> 3
@@ -104,7 +102,6 @@ func handleRegToFromRegMem() {
 	// R/M
 	rm_bits := b2 & 0b00000111
 
-	// TODO: the below is just the initial homework case (mod == 11), rework for other mod cases
 	var reg_str string
 	if w_bit_on {
 		reg_str = w_on_map[reg_bits]
@@ -113,18 +110,106 @@ func handleRegToFromRegMem() {
 	}
 
 	var rm_str string
-	if w_bit_on {
-		rm_str = w_on_map[rm_bits]
-	} else {
-		rm_str = w_off_map[rm_bits]
+
+	// Register Mode (Register to Register)
+	if mod_bits == 0b11 {
+		if w_bit_on {
+			rm_str = w_on_map[rm_bits]
+		} else {
+			rm_str = w_off_map[rm_bits]
+		}
+
+		// d_bit == 0 --> REG is NOT the Dest --> R/M REG
+		// d_bit == 1 --> REG IS the Dest 	  --> REG R/M
+		if d_bit_on {
+			builder.WriteString(fmt.Sprintf("%s, %s", reg_str, rm_str))
+		} else {
+			builder.WriteString(fmt.Sprintf("%s, %s", rm_str, reg_str))
+		}
+		return
+	}
+
+	// Effective Address Calc
+
+	// Check for special Direct Address Case
+	if mod_bits == 0b00 && rm_bits == 0b110 {
+		i++
+		b3 := prog[i]
+		i++
+		b4 := prog[i]
+		disp := uint16(b4) | (uint16(b3) << 8)
+
+		builder.WriteString(fmt.Sprintf("[%d]", disp))
+		return
+	}
+
+	base_str := rm_effective_map[rm_bits]
+
+	// No Displacement, just end instruction
+	if mod_bits == 0b00 {
+		rm_str = fmt.Sprintf("[%s]", base_str)
+	} else if mod_bits == 0b01 { // 8-bit Displacement
+		i++
+		b3 := prog[i]
+		disp := uint8(b3)
+
+		// The only way to use bp without an offset is to have a zero offset, b/c of the special
+		// direct address case above
+		if disp == 0 {
+			rm_str = fmt.Sprintf("[%s]", base_str)
+		} else {
+			rm_str = fmt.Sprintf("[%s + %d]", base_str, disp)
+		}
+	} else if mod_bits == 0b10 { // 16-bit Displacement
+		i++
+		b3 := prog[i]
+		i++
+		b4 := prog[i]
+		disp := uint16(b3) | (uint16(b4) << 8)
+
+		if disp == 0 {
+			rm_str = fmt.Sprintf("[%s]", base_str)
+		} else {
+			rm_str = fmt.Sprintf("[%s + %d]", base_str, disp)
+		}
 	}
 
 	// d_bit == 0 --> REG is NOT the Dest --> R/M REG
 	// d_bit == 1 --> REG IS the Dest 	  --> REG R/M
 	if d_bit_on {
-		builder.WriteString(fmt.Sprintf("%s %s", reg_str, rm_str))
+		builder.WriteString(fmt.Sprintf("%s, %s", reg_str, rm_str))
 	} else {
-		builder.WriteString(fmt.Sprintf("%s %s", rm_str, reg_str))
+		builder.WriteString(fmt.Sprintf("%s, %s", rm_str, reg_str))
 	}
-	builder.WriteString("\n")
+}
+
+// MOV 3
+func handleImmediateToReg() {
+	builder.WriteString("mov ")
+
+	w_bit_on := b&0b00001000 != 0
+	reg_bits := b & 0b00000111
+
+	var reg_str string
+	if w_bit_on {
+		reg_str = w_on_map[reg_bits]
+	} else {
+		reg_str = w_off_map[reg_bits]
+	}
+
+	builder.WriteString(fmt.Sprintf("%s, ", reg_str))
+
+	i++
+	b2 := prog[i]
+
+	if w_bit_on {
+		i++
+		b3 := prog[i]
+
+		value := uint16(b2) | (uint16(b3) << 8)
+
+		builder.WriteString(fmt.Sprintf("%d", value))
+	} else {
+		builder.WriteString(fmt.Sprintf("%d", b2))
+	}
 }
