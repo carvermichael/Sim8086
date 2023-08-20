11b4a153eedac778e6faad58e2d0a0a9ebd50f82
Michael Carver <carvermichael@gmail.com>
2023-08-20 11:26:40 -0500

    quick reorg, setting up for simple test(s)

---
+++ cmd/Sim8086/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	. "github.com/carvermichael/Sim8086/internal"
+	"log"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) <= 1 {
+		log.Panic("you need a fileName, ya dum dum!")
+	}
+
+	fileName := os.Args[1]
+	fmt.Println(fileName)
+
+	asm_string := GetASMFromFile(fileName)
+
+	fmt.Print(asm_string)
+
+	return
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/carvermichael/Sim8086
+
+go 1.18
